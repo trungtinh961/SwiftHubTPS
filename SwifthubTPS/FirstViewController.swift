bb7d70b73ca1b04ceda9d3eff71e9920c01898c8
TPS <hung.thai@tpssoft.com>
2020-08-24 16:34:53 +0700

    Initial Commit

---
+++ SwifthubTPS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SwifthubTPS
+//
+//  Created by TPS on 8/24/20.
+//  Copyright © 2020 Trung Tinh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwifthubTPS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SwifthubTPS
+//
+//  Created by TPS on 8/24/20.
+//  Copyright © 2020 Trung Tinh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
