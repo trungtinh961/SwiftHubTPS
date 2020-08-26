ab423dd3748b97ebb12b94b832d1130d145f9866
TPS <hung.thai@tpssoft.com>
2020-08-26 09:41:19 +0700

    Add pod

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'SwifthubTPS' do
+    pod 'HMSegmentedControl'
+end
--- SwifthubTPS/Modules/FirstViewController.swift
+++ SwifthubTPS/Modules/FirstViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import HMSegmentedControl
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        let segmentedControl = HMSegmentedControl(sectionTitles: [
+//            "Trending",
+//            "News",
+//            "Library"
+//        ])
+//
+//        segmentedControl.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
+//        segmentedControl.addTarget(self, action: #selector(segmentedControlChangedValue(segmentedControl:)), for: .valueChanged)
+//        view.addSubview(segmentedControl)
     }
-
-
+    
 }
 
