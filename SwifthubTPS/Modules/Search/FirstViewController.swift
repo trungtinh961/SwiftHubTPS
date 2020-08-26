f2c4312296a9e662c40fe8afe0bfbcbcca6b7e1a
TPS <hung.thai@tpssoft.com>
2020-08-26 10:06:21 +0700

    .

---
+++ SwifthubTPS/Modules/Search/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  SwifthubTPS
+//
+//  Created by TPS on 8/24/20.
+//  Copyright © 2020 Trung Tinh. All rights reserved.
+//
+
+import UIKit
+import HMSegmentedControl
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
+
